3f3cf2998c233016aaf467758761930283ed0689
Dex Coder <60599437+mhdkashif07@users.noreply.github.com>
2022-12-26 16:45:02 +0500

    updated code

--- src/components/buttons/PrimaryButton.tsx
+++ src/components/buttons/PrimaryButton.tsx
@@ -1,13 +1,20 @@
 import React, { FC } from 'react'
 
-const PrimaryButton:FC<{text: string}> = ({ text }) => {
+type buttonProps = {
+  text: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void; // to handle onClick functions
+  children?: React.ReactNode; // make the component able to receive children elements
+  color?: 'primary' | 'secondary'; // two styling options (you can create as many as you want)
+  disabled?: boolean; // make the button disabled or not
+}
+const PrimaryButton:FC<buttonProps> = ({ text, onClick, children, color, disabled=false }) => {
   return (
     <div className='button__container'>
         <div className='primary__button'>
-            <button>{text}</button>
+            <button onClick={onClick} disabled={disabled} >{text}</button>
         </div>
     </div>
   )
 }
 
-export default PrimaryButton
+export default PrimaryButton;
