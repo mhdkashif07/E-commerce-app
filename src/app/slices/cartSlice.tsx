9db88abf64074608317a7be567374260640dc516
Dex Coder <60599437+mhdkashif07@users.noreply.github.com>
2023-03-14 11:24:03 +0500

    cart issues solved

--- src/app/slices/cartSlice.tsx
+++ src/app/slices/cartSlice.tsx
@@ -28,11 +28,15 @@ const cartSlice = createSlice({
   reducers: {
     addToCart(state, action) {
       console.log(action.payload);
-      
-      //we are checking if the item is already in the cart
+
+      //*we are checking if the item is already in the cart
       const itemIndex = state.cartItems.findIndex(
-        (item: { id: number; }) => item.id === action.payload.id
+        (item: {
+          articlesList: any; code: number;
+        }) => item.code == action.payload.articlesList?.[0]?.code
       );
+      console.log(itemIndex);
+
 
       //we are checking if the item is already in the cart we will increase the quantity of the item
       if (itemIndex >= 0) {
@@ -57,7 +61,7 @@ const cartSlice = createSlice({
 
     removeFromCart(state, action) {
       const nextCartItems = state.cartItems.filter(
-        (item: { id: any; }) => item.id !== action.payload.id
+        (item: { code: any; }) => item.code !== action.payload.code
       );
       state.cartItems = nextCartItems;
       localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
@@ -77,9 +81,14 @@ const cartSlice = createSlice({
     },
 
     decreaseCart(state, action) {
-      const itemIndex = state.cartItems.findIndex(
-        (item: { id: any; }) => item.id === action.payload.id
-      );
+      console.log(action.payload);
+
+      console.log(state.cartItems[0].code);
+
+
+      const itemIndex = state.cartItems.findIndex((item: { code: any; }) => item.code === action.payload.code);
+      console.log(itemIndex);
+
 
       if (state.cartItems[itemIndex].cartQuantity > 1) {
         state.cartItems[itemIndex].cartQuantity -= 1;
@@ -89,7 +98,7 @@ const cartSlice = createSlice({
         });
       } else if (state.cartItems[itemIndex].cartQuantity === 1) {
         const nextCartItems = state.cartItems.filter(
-          (item: { id: any; }) => item.id !== action.payload.id
+          (item: { code: any; }) => item.code !== action.payload.code
         );
         state.cartItems = nextCartItems;
 
@@ -102,14 +111,16 @@ const cartSlice = createSlice({
     },
 
     getTotals(state, action: PayloadAction) {
-      //reduce array methods accepts two parameters first is callback function and sectond is initial value 
+      //reduce array methods accepts two parameters first is callback function and second is initial value 
       //cartTotal will hold the initial values of total and quantity
       //cartItem will be the item which we will give each time of iteration
-      let { total, quantity } = state.cartItems.reduce((cartTotal: { total: number; quantity: number; }, cartItem: { price: number; cartQuantity: number; }) => {
+      let { total, quantity } = state.cartItems.reduce((cartTotal: { total: number; quantity: number; }, cartItem: { whitePrice: { price: number }; cartQuantity: number; }) => {
         //this is callback function
 
         //destructure the price and cartQuantity from cartItem
-        const { price, cartQuantity } = cartItem
+        const { whitePrice: { price }, cartQuantity } = cartItem
+        console.log(price);
+
 
         //itemTotal will store the total amount each of the item
         const itemTotal = price * cartQuantity // we will multiply the price of the item to the quantity of the item
